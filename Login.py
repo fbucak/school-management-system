58c2606de37c2fa12182b1aa9b44069c59958a07
Hakan Gokalp <gokalphakan@gmail.com>
2022-06-21 00:19:55 +0200

    logo eklendi

--- Login.py
+++ Login.py
@@ -2,6 +2,7 @@ from PyQt5 import QtWidgets,uic
 import studentLogin
 import teacherLogin
 from DbConnect import DBConnection
+from PyQt5.QtGui import QPixmap
 
 
 class login(QtWidgets.QMainWindow):
@@ -9,6 +10,8 @@ class login(QtWidgets.QMainWindow):
     def __init__(self,db):
         super(login, self).__init__()
         uic.loadUi('ui/loginpage.ui', self)
+        qpixmap = QPixmap('untitled-10.png')
+        self.logo.setPixmap(qpixmap)
         self.db=db
         self.forStudent.clicked.connect(self.StudentLoginPage)
         self.forTeacher.clicked.connect(self.TeacherLoginPage)
